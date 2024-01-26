34f778374be01e0ba4c8f4b8a4de8de2af9951f1
ZQR0 <yarikrech@gmail.com>
2024-01-26 21:52:05 +0300

    design changes, new functioonallity for bluetooth

--- app/src/main/java/com/morze/morzetransfer/MainActivityJava.java
+++ app/src/main/java/com/morze/morzetransfer/MainActivityJava.java
@@ -1,22 +1,28 @@
 package com.morze.morzetransfer;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.hardware.lights.LightState;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +34,8 @@ import com.morze.morzetransfer.bluetooth.BluetoothService;
 import com.morze.morzetransfer.bluetooth.BluetoothServiceManager;
 import com.morze.morzetransfer.converter.MorseTranslator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -36,6 +44,8 @@ public class MainActivityJava extends Activity {
     // Constants
     private static final String TAG = "MainActivityJava";
     private static final int REQUEST_ENABLE_BT = 1;
+
+    private final FragmentManager fragmentManager = this.getFragmentManager();
 
     // Bluetooth objects
     private BluetoothAdapter adapter;
@@ -47,8 +57,12 @@ public class MainActivityJava extends Activity {
     // Elements from activity xml
     private ImageView settingsImage;
     private Button sendButton;
+    private TextView deviceNameField;
     private TextView morseTextField;
     private EditText inputField;
+    private FrameLayout frameLayout;
+    private ListView deviceList;
+    private ImageView closeButton;
 
 
     @Override
@@ -62,24 +76,14 @@ public class MainActivityJava extends Activity {
         this.initAllElements();
         BluetoothPermissionsUtils.requestPermissionsForBluetooth(this, this);
         this.enableBluetoothFromActivity();
-        this.switchToSettings();
         this.translateAndShow();
-        this.registerAllReceivers();
+        this.openDevicesList();
+        this.closeOverlayButtonOnClick();
 
         this.manager = new BluetoothServiceManager(this, handler, this);
         Log.d(TAG, "Bluetooth manager initialized");
-
-        //ONLY FOR DEBUGGING
-        // TODO: 12.12.2023 remove after debugging and final tests
-        String deviceName = this.getDeviceNameFromIntentExtras();
-        if (deviceName != null) {
-            Toast.makeText(this, deviceName, Toast.LENGTH_SHORT).show();
-            this.chooseConnectionMethod(deviceName);
-            Log.d(TAG, "Connection method in choosing");
-        } else {
-            Toast.makeText(this, "Пусто", Toast.LENGTH_SHORT).show();
-        }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -94,11 +98,12 @@ public class MainActivityJava extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+
     @Override
     protected void onResume() {
-        this.registerAllReceivers();
         super.onResume();
     }
+
 
     @Override
     protected void onPause() {
@@ -116,6 +121,7 @@ public class MainActivityJava extends Activity {
         super.onPause();
     }
 
+
     @Override
     protected void onDestroy() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
@@ -132,29 +138,46 @@ public class MainActivityJava extends Activity {
         super.onDestroy();
     }
 
+
     private void initAllElements() {
-        settingsImage = (ImageView) findViewById(R.id.settings_image);
+//        settingsImage = (ImageView) findViewById(R.id.settings_image);
         inputField = (EditText) findViewById(R.id.input_field);
         sendButton = (Button) findViewById(R.id.sendButton);
         morseTextField = (TextView) findViewById(R.id.morze_output_field);
+        deviceNameField = findViewById(R.id.device_name);
+        deviceList = findViewById(R.id.list_of_devices);
+        frameLayout = findViewById(R.id.overlay_fragment_devices_list);
+        closeButton = findViewById(R.id.overlay_close_button);
     }
 
 
-    private void switchToSettings() {
-        settingsImage.setOnClickListener(new View.OnClickListener() {
+    private void openDevicesList() {
+        this.deviceNameField.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeActivity(SettingsActivityJava.class);
-                finish();
-                Log.d(TAG, "onClick: pressed");
+                Log.d(TAG, "Device name field clicked");
+                showBluetoothListDialog();
             }
         });
     }
+
+//    private void switchToSettings() {
+//        settingsImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                changeActivity(SettingsActivityJava.class);
+//                finish();
+//                Log.d(TAG, "onClick: pressed");
+//            }
+//        });
+//    }
+
 
     private void changeActivity(Class<?> destinationActivity) {
         Intent intent = new Intent(MainActivityJava.this, destinationActivity);
         startActivity(intent);
     }
+
 
     // Здесь происходит и перевод, и вывод в TextView, и отправление сообщения
     private void translateAndShow() {
@@ -175,6 +198,9 @@ public class MainActivityJava extends Activity {
     }
 
 
+    /**
+     * Method for enabling bluetooth from activity, if it's not enabled yet
+     * */
     private void enableBluetoothFromActivity() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
             BluetoothPermissionsUtils.requestPermissionsForBluetooth(this, this);
@@ -190,74 +216,6 @@ public class MainActivityJava extends Activity {
             }
         } else if (adapter == null) {
             Toast.makeText(this, Constants.NOT_SUPPORT, Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void registerAllReceivers() {
-        IntentFilter discoveryFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(this.discoveryReciever, discoveryFilter);
-    }
-
-    private String getDeviceNameFromIntentExtras() {
-        Bundle bundle = getIntent().getExtras();
-
-        // FIXME: 11.12.2023 getString method always returns null
-        if (bundle != null) {
-            return bundle.getString(Constants.BLUETOOTH_DEVICE);
-        }
-
-        return null;
-    }
-
-
-    private void chooseConnectionMethod(String deviceName) {
-        if (deviceName != null) {
-            BluetoothDevice deviceFromBonded = this.getTargetDeviceFromBonded(deviceName);
-            if (deviceFromBonded != null) {
-                this.manager.connectDevice(deviceFromBonded, false);
-                Log.d(TAG, "Device found in bonded");
-            }
-
-            this.startDeviceDiscovery(deviceName);
-            Log.d(TAG, "Device discovery started");
-        } else {
-            Log.e(TAG, "deviceName is null");
-        }
-    }
-
-    private void startDeviceDiscovery(String deviceName) {
-        // Регистрация BroadcastReceiver для обнаружения устройств Bluetooth
-        this.discoveryReciever = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (ActivityCompat.checkSelfPermission(MainActivityJava.this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-                    BluetoothPermissionsUtils.requestPermissionsForBluetooth(MainActivityJava.this, MainActivityJava.this);
-                }
-                final String action = intent.getAction();
-                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                    Log.d(TAG, "Action is ACTION_FOUND");
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    if (device != null) {
-                        Log.d(TAG, "Device is not null");
-                        if (device.getName().equals(deviceName)) {
-                            Toast.makeText(context, "Подключено к устройству: " + device.getName(), Toast.LENGTH_SHORT).show();
-                            BluetoothDevice newDevice = adapter.getRemoteDevice(device.getAddress());
-                            manager.connectDevice(device, false);
-                            Log.d(TAG, String.format("Device with name %s and MAC-address %s connected by BluetoothManager class", device.getName(), device.getAddress()));
-
-                            if (adapter.cancelDiscovery()) {
-                                Log.d(TAG, "Discovery cancelled by adapter");
-                            }
-                        }
-                    }
-
-                    Log.e(TAG, "Device is null, couldn't find the device");
-                }
-            }
-        };
-
-        if (this.adapter.startDiscovery()) {
-            Log.d(TAG, "Discovery started by BluetoothAdapter");
         }
     }
 
@@ -287,11 +245,70 @@ public class MainActivityJava extends Activity {
         return null;
     }
 
+
+
+    /**
+     * @return set of paired bluetooth devices
+     * */
     private Set<BluetoothDevice> getBondedDevices() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
             BluetoothPermissionsUtils.requestPermissionsForBluetooth(this, this);
         }
         return this.adapter.getBondedDevices();
+    }
+
+
+    /**
+     * Method for showing the list of paired devices as the view
+     * */
+    private void showBluetoothListDialog() {
+        Log.d(TAG, "");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            BluetoothPermissionsUtils.requestPermissionsForBluetooth(this, this);
+        }
+
+        this.frameLayout.setVisibility(View.VISIBLE); // Делаем FrameLayout видимым для пользователя
+
+        List<String> deviceNames = new ArrayList<>();
+        List<BluetoothDevice> devices = new ArrayList<>();
+
+        for (BluetoothDevice device : this.getBondedDevices()) {
+            deviceNames.add(device.getName());
+            devices.add(device);
+        }
+
+        //Сделать предупреждение о пустом и не пустом списке через виджеты Toast
+        if (devices.size() == 0) {
+            Toast.makeText(this, "Устройств не обнаружено", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Устройства найдены", Toast.LENGTH_SHORT).show();
+        }
+
+        final String[] deviceArray = deviceNames.toArray(new String[deviceNames.size()]);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceArray);
+
+        this.deviceList.setAdapter(arrayAdapter);
+        this.deviceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = (String) adapterView.getItemAtPosition(i);
+                Log.d(TAG, String.format("Выбран элемент %s", selectedItem));
+                /// TODO: 26.01.2024 connection to devices
+            }
+        });
+    }
+
+    /**
+     * Method for overlay closing
+     * */
+    private void closeOverlayButtonOnClick() {
+        this.closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                frameLayout.setVisibility(View.INVISIBLE);
+                Log.d(TAG, "FrameLayout is invisible");
+            }
+        });
     }
 
 
